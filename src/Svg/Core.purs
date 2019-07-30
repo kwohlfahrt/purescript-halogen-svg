c95ec850f00adf77323e22a7427ec1cd2a603915
Rowan Skewes <rowan.skewes@gmail.com>
2019-07-30 20:33:37 +1000

    Keyed elements

--- src/Svg/Core.purs
+++ src/Svg/Core.purs
@@ -3,8 +3,9 @@ module Core where
 
 import Prelude
 import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple)
 import Halogen.HTML.Core (HTML, Prop(Attribute), Namespace(Namespace), AttrName(AttrName))
-import Halogen.VDom (ElemName, ElemSpec(ElemSpec), VDom(Elem))
+import Halogen.VDom (ElemName, ElemSpec(ElemSpec), VDom(Elem, Keyed))
 import Unsafe.Coerce (unsafeCoerce)
 
 ns :: Maybe Namespace
@@ -19,3 +20,10 @@ element = coe (\name props children -> Elem (ElemSpec ns name props) children)
 
 attr :: forall i. AttrName -> String -> Prop i
 attr (AttrName name) = Attribute Nothing name
+
+keyedElement :: forall p i. ElemName -> Array (Prop i) -> Array (Tuple String (HTML p i)) -> HTML p i
+keyedElement = coe (\name props children -> Keyed (ElemSpec ns name props) children)
+  where
+    coe :: (ElemName -> Array (Prop i) -> Array (Tuple String (VDom (Array (Prop i)) p)) -> VDom (Array (Prop i)) p)
+        -> ElemName -> Array (Prop i) -> Array (Tuple String (HTML p i)) -> HTML p i
+    coe = unsafeCoerce
