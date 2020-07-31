cad5b738d821fad12140c6fd0b1c5f8e98eba614
Serhii Khoma <srghma@gmail.com>
2020-07-31 18:29:03 +0300

    feat: update npm packages, use spago, remove psc-package.json, update travis

--- packages.dhall
+++ packages.dhall
@@ -119,7 +119,7 @@ let additions =
 
 
 let upstream =
-      https://github.com/purescript/package-sets/releases/download/psc-0.13.4-20191025/packages.dhall sha256:f9eb600e5c2a439c3ac9543b1f36590696342baedab2d54ae0aa03c9447ce7d4
+      https://github.com/purescript/package-sets/releases/download/psc-0.13.8-20200724/packages.dhall sha256:bb941d30820a49345a0e88937094d2b9983d939c9fd3a46969b85ce44953d7d9
 
 let overrides = {=}
 
--- src/Svg/Core.purs
+++ src/Svg/Core.purs
@@ -5,7 +5,7 @@ import Prelude
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple)
 import Halogen.HTML.Core (HTML, Prop(Attribute), Namespace(Namespace), AttrName(AttrName))
-import Halogen.VDom (ElemName, ElemSpec(ElemSpec), VDom(Elem, Keyed))
+import Halogen.VDom (ElemName, VDom(Elem, Keyed))
 import Unsafe.Coerce (unsafeCoerce)
 
 ns :: Maybe Namespace
@@ -26,7 +26,7 @@ attr :: forall i. AttrName -> String -> Prop i
 attr (AttrName name) = Attribute Nothing name
 
 keyedElement :: forall p i. ElemName -> Array (Prop i) -> Array (Tuple String (HTML p i)) -> HTML p i
-keyedElement = coe (\name props children -> Keyed (ElemSpec ns name props) children)
+keyedElement = coe (\name props children -> Keyed ns name props children)
   where
     coe :: (ElemName -> Array (Prop i) -> Array (Tuple String (VDom (Array (Prop i)) p)) -> VDom (Array (Prop i)) p)
         -> ElemName -> Array (Prop i) -> Array (Tuple String (HTML p i)) -> HTML p i
